48ce3dfa5187f96b2e41cff145eac804832fe0ba
17matb <147427088+17matb@users.noreply.github.com>
2025-11-20 14:25:38 +0100

    ops: use azureml-core instead of azureml-mlflow to connect to azure ml workspace

--- backend/ml/train.py
+++ backend/ml/train.py
@@ -1,11 +1,11 @@
-import os
 from pathlib import Path
 
 import joblib
 import mlflow
+from azureml.core import Workspace
 from dotenv import load_dotenv
 from logs.logger import get_logger
-from mlflow import exceptions, sklearn
+from mlflow import sklearn
 from sklearn import datasets
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
@@ -17,42 +17,16 @@ logger = get_logger(__name__)
 BASE_DIR = Path(__file__).resolve().parent
 MODEL_PATH = BASE_DIR.parent / "model" / "model.pkl"
 logger.info(f"Model will be saved in {MODEL_PATH}")
-DB_PATH = BASE_DIR / "mlruns.db"
-logger.info(f"Database will be saved in {DB_PATH}")
-ARTIFACT_PATH = BASE_DIR / "mlruns"
-logger.info(f"Artifact directory will be at {ARTIFACT_PATH}")
 
-MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "")
+ws = Workspace.from_config()
+tracking_uri = ws.get_mlflow_tracking_uri()
 
-if (
-    MLFLOW_TRACKING_URI
-    and MLFLOW_TRACKING_URI.startswith("azureml://")
-    and os.getenv("GITHUB_ACTIONS") == "true"
-):
-    logger.info("Using Azure ML Tracking Server")
-    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-else:
-    logger.info(f"Using local SQLite tracking: sqlite:///{DB_PATH}")
-    mlflow.set_tracking_uri(f"sqlite:///{DB_PATH}")
+logger.info("Using Azure ML Tracking Server")
+mlflow.set_tracking_uri(tracking_uri)
 
 experiment_name = "MLFlow quickstart"
 
-if (
-    MLFLOW_TRACKING_URI
-    and MLFLOW_TRACKING_URI.startswith("azureml://")
-    and os.getenv("GITHUB_ACTIONS") == "true"
-):
-    try:
-        mlflow.create_experiment(experiment_name)
-    except exceptions.MlflowException:
-        pass
-else:
-    try:
-        mlflow.create_experiment(
-            experiment_name, artifact_location=ARTIFACT_PATH.as_uri()
-        )
-    except exceptions.MlflowException:
-        pass
+mlflow.create_experiment(experiment_name)
 
 
 mlflow.set_experiment(experiment_name)
